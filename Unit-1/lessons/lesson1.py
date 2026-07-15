0223d89189df3ab18c4687d1423cfa553d340389
Samkeliso <lainne_coatzee@icloud.com>
2026-07-15 14:30:54 +0200

    Adding folders

---
+++ Unit-1/lessons/lesson1.py
@@ -0,0 +1,11 @@
+# Making the computer speak
+#print("Hello, World!")
+
+
+# Storing in a variable
+#name = "Samkeliso"
+#print(name)
+
+# Making interactive code 
+name = input("Enter your name: ")
+print(f"Greetings, {name}")
---
+++ Unit-1/tasks/Pratical-task/student_info.py
@@ -0,0 +1,15 @@
+print ("===========================================")
+print ("               Student Card                ")
+print ("===========================================")
+
+name = input("What is you first name: ")
+surname = input("Enter your surname:")
+age = input("How old are you? : ")
+number_float = input("What is you favorite number? ")
+
+
+print(f"Welcome, {name}!")
+
+
+
+
